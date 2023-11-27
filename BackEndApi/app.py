2922b021931c556f5ac90108b7495a1730d88948
Kevin Delgado <kevindel891@gmail.com>
2023-11-26 16:53:46 -0800

    Combine temp rubik solver with backend api

--- BackEndApi/app.py
+++ BackEndApi/app.py
@@ -6,6 +6,8 @@ import os
 import sys
 import cv2
 import numpy as np
+from rubik.cube import Cube
+from rubik.solve import Solver
 
 
 app = Flask(__name__)
@@ -56,7 +58,7 @@ def getColors():
         for rowStart in range(0, 300, 100):
             rowEnd = rowStart + 100
             for colStart in range(0, 300, 100):
-                colorFound = False
+                #colorFound = False
                 colEnd = colStart + 100
                 #Get Center of cube face
                 (cX, cY) = (colEnd, rowEnd)
@@ -64,14 +66,14 @@ def getColors():
 
                 #slice image to only get  middle
                 t1 = image[rowStart:cY, colStart:cX]
-                (h,w) = t1.shape[:2]
+                #(h,w) = t1.shape[:2]
                 t2 = t1[25:75, 25:75]
                 for colors in colorBounds:            
                     lower = colorBounds[colors][0]
                     upper = colorBounds[colors][1]
 
                     mask = cv2.inRange(t2, lower, upper)
-                    detected_output = cv2.bitwise_and(t2, t2, mask=mask)
+                    #detected_output = cv2.bitwise_and(t2, t2, mask=mask)
                     found = cv2.countNonZero(mask)
                     firstKey = list(facecolors)[picture]
                     if(found > 60):
@@ -100,3 +102,81 @@ def image():
             print(tempcolors)
             return jsonify(tempcolors)
         return "Image read"
+
+
+def fetch_cube_state_as_str(json_orientation):
+    string_orientation = ""
+    other_faces = ["left", "front", "right", "back"]
+    
+    start_index = 0
+    end_index = 3
+
+    for square in json_orientation["top"]:
+        string_orientation += square[0].upper()
+
+    while (end_index <= 9):
+        for face in other_faces:
+            color_set = json_orientation[face][start_index:end_index]
+            for color in color_set:
+                string_orientation += color[0].upper()
+        
+        start_index += 3
+        end_index += 3
+    
+    for square in json_orientation["bottom"]:
+        string_orientation += square[0].upper()
+
+    return string_orientation
+
+def convert_solver_moves(solver_moves):
+    legend = {
+        "U": "Turn the upper face clockwise (90 degrees)",
+        "Ui": "Turn the upper face counterclockwise (90 degrees)",
+        "B": "Turn the back face clockwise (90 degrees)",
+        "Bi": "Turnthe back face counterclockwise (90 degrees)",
+        "E": "Turn the Equatorial slice clockwise (90 degrees)",
+        "Ei": "Turn the Equatorial slice counterclockwise (90 degrees)",
+        "L": "Turn the Left face clockwise (90 degrees)",
+        "Li": "Turn the Left face counterclockwise (90 degrees)",
+        "R": "Turn the Right face clockwise (90 degrees)",
+        "Ri": "Turn the Right face counterclockwise (90 degrees)",
+        "D": "Turn the Down face clockwise (90 degrees)",
+        "Di": "Turn the Down face counterclockwise (90 degrees)",
+        "F": "Turn the Front face clockwise (90 degrees)",
+        "Fi": "Turn the Front face counterclockwise (90 degrees)",
+        "S": "Turn the middle vertical slice clockwise (90 degrees)",
+        "Si": "Turn the middle vertical slice counterclockwise (90 degrees)",
+        "X": "Rotate the entire cube around the X-axis clockwise (viewed from the front)",
+        "Xi": "Rotate the entire cube around the X-axis counterclockwise (viewed from the front)",
+        "Z": "Rotate the entire cube around the Z-axis clockwise (viewed from the front)",
+        "Zi": "Rotate the entire cube around the Z-axis counterclockwise (viewed from the front)"
+    }
+
+    converted_moves = []
+
+    for move in solver_moves:
+        converted_moves.append(legend[move])
+    
+    return converted_moves
+
+@app.route("/steps", methods=['GET', 'POST'])
+def steps():
+    if request.method == "POST":
+        json_orientation = request.json["imageValues"]
+        print(json_orientation)
+        string_orientation = fetch_cube_state_as_str(json_orientation)
+
+        cube = Cube(string_orientation)
+        print(cube)
+        solver = Solver(cube)
+        solver.solve()
+
+        raw_moves = solver.moves
+        converted_moves = convert_solver_moves(raw_moves)
+
+        steps = {
+            "raw_moves": raw_moves,
+            "converted_moves": converted_moves
+        }
+
+        return jsonify(steps)
